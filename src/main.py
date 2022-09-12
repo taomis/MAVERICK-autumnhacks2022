947bb7c7809386cb3422360777ae228b8e81aed0
thomas-w-0 <87916443+thomas-w-0@users.noreply.github.com>
2022-09-12 15:54:05 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-import discord
+import discord, dependencies as d
 from discord import app_commands
 from discord.ext import commands
 
-guild_id = 486327698751488016
+guild_id = 0 # change it to your server's id
 
 class MyClient(discord.Client):
     def __init__(self, *, intents: discord.Intents):
@@ -19,7 +19,7 @@ client = MyClient(intents=intents)
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user} (ID: {client.user.id})')
-    print('------')
+
 
 @client.tree.command()
 async def help(interaction: discord.Interaction):
@@ -31,20 +31,24 @@ async def help(interaction: discord.Interaction):
     #embed.set_author(name = "MAVERICK", icon_url = "image url")
     #embed.set_image(url = "image url")
     #embed.set_thumbnail(url = "https://media.discordapp.net/attachments/967987262048567306/1018742994310008872/unknown.png")
-    embed.add_field(name = "setrem *time_zone* *time*", value = "0")
     embed.add_field(name = "CALc **", value = "0")
     #embed.footer(text = "This is a footer")
 
     await interaction.response.send_message(embed = embed, ephemeral=True)
     
+@client.tree.command()
+@app_commands.rename(weight='weight')
+@app_commands.rename(height='height')
+@app_commands.rename(age='age')
+@app_commands.rename(selection='selection')
+async def exercise(interaction: discord.Interaction, weight: str, height: str, age: str, selection: str):
+    await interaction.response.send_message('The amount of calories you need is to maintain Weight is ' + str(d.type_of_exercise(weight, height, age, selection)))
 
-#@client.tree.command()
-async def setrem():
-    pass
-
-#@client.tree.command()
-async def CALc():
-    pass
+@app_commands.rename(weight='weight')
+@app_commands.rename(height='height')
+@client.tree.command()
+async def bmi(interaction: discord.Interaction, weight: str, height: str):
+    await interaction.response.send_message(str('Your body mass index is ' + str(d.bmi(weight, height)) + '\n' + str(d.bmi_2(weight, height))))
 
 
-client.run('MTAxODM0ODQzNTYwNDI0MjQ4NA.GW-fey.pN8W-_7mNo18fK_JB85l3L8qQps_3ua-NtCWNs')
+client.run('token')
