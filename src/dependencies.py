11a1641ee8d3089240aa74c5bc01ad2b1d9ff830
thomas-w-0 <87916443+thomas-w-0@users.noreply.github.com>
2022-09-12 15:54:34 -0700

    Update dependencies.py

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,25 +1,26 @@
 
-def type_of_exercise(weight: float, height: float, ageInput: float, selection: str):
+def type_of_exercise(weight: str, height: str, ageInput: str, selection: str):
     if selection == "sedentary":
-        return float((13.397*(weight)+ 4.799*(height) - 5.677*(ageInput) + 88.362)* 1.2)
+        return float((13.397*(float(weight))+ 4.799*(float(height)) - 5.677*(float(ageInput)) + 88.362)* 1.2)
     elif selection == "light":
-        return float((13.397*(weight)+ 4.799*(height) - 5.677*(ageInput) + 88.362)*1.35)
+        return float((13.397*(float(weight))+ 4.799*(float(height)) - 5.677*(float(ageInput)) + 88.362)*1.35)
     elif selection == "moderate:":
-        return float((13.397*(weight)+ 4.799*(height) - 5.677*(ageInput) + 88.362)*1.55)
+        return float((13.397*(float(weight))+ 4.799*(float(height)) - 5.677*(float(ageInput)) + 88.362)*1.55)
     elif selection == "heavy":
-        return float((13.397*(weight)+ 4.799*(height) - 5.677*(ageInput) + 88.362)*1.7)
+        return float((13.397*(float(weight))+ 4.799*(float(height)) - 5.677*(float(ageInput)) + 88.362)*1.7)
     elif selection == "very":
-        return float((13.397*(weight)+ 4.799*(height) - 5.677*(ageInput) + 88.362)*1.9)
+        return float((13.397*(float(weight))+ 4.799*(float(height)) - 5.677*(float(ageInput)) + 88.362)*1.9)
 
-def bmi(weight: float, height: float):
-    return ((weight/(height**2))*10000) 
+def bmi(weight: str, height: str):
+    return ((float(weight)/(float(height)**2))*10000) 
 
-def bmi_2(weight: float, height: float):
-    if bmi(weight, height) < 18.5:
-        return("You are Underweight")
-    elif bmi(weight, height) < 24.9:
+def bmi_2(weight: str, height: str):
+    if bmi(float(float(weight)), float(float(height))) < 18.5:
+        return("You are Underfloat(weight)")
+    elif bmi(float(weight), float(height)) < 24.9:
         return("You have a normal body mass index")
-    elif bmi(weight, height) < 29.9:
-        return("You are Overweight")
-    elif bmi(weight, height) > 29.91:
+    elif bmi(float(weight), float(height)) < 29.9:
+        return("You are Overfloat(weight)")
+    elif bmi(float(weight), float(height)) > 29.91:
         return("You are Obese")
+
